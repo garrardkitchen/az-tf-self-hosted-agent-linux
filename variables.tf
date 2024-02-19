30fc7a6c4898c1d4a6f4303cb6a217e46a7dec75
Garrard Kitchen <garrardkitchen@gmail.com>
2024-02-19 21:12:31 +0000

    refactor: Added descriptions, fixed typos and removed commented out code (#2)

--- variables.tf
+++ variables.tf
@@ -1,25 +1,31 @@
 variable "rg" {
+  description = "The Resource Group where these resouces will be created"
   type = string
 }
 
 variable "vnet" {
+  description = "The name of the VNET that (a) will be created and 9b) where the self hosted agent will exist"
   type = string
 }
 
 variable "subnet" {
+  description = "The name of the subnet where the self hosted agent will exist"
   type = string
 }
 
 variable "vm" {
+  description = "The name of the self hosted agent VM"
   type = string
 }
 
 variable "computer_name" {
+  description = "The hostname of the self hosted agent"
   type    = string
   default = "garrard-tf-vm"
 }
 
 variable "sa" {
+  description = "The name of the Storage Account where the remote state will persist"
   type = string
 }
 
@@ -41,12 +47,12 @@ variable "allow_outbound_cidr_blocks" {
   default     = ["0.0.0.0/0"]
 }
 variable "agent_name" {
-  description = "The name of the agent"
+  description = "The name of the ADO agent"
   type        = string
   default     = "agent-"
 }
 variable "pool_name" {
-  description = "The name of the pool"
+  description = "The name of the ADO pool where the ADO agent will live"
   type        = string
   default     = "tf-pool"
 }
